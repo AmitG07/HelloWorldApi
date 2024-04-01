4eb2736f38146fe83edfe785d85bf3a07ee58b4a
amit04 <amit04@nagarro.com>
2024-04-01 15:43:04 +0530

    First Commit

---
+++ ProjectDevOps/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectDevOps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello World";
+        }
+    }
+}
